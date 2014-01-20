d0cbb6140da5f0aab617e37dd18c53144b1980b7
preethivasanth <preethiv.cse@gmail.com>
2014-01-20 20:48:49 +0530

    Create connects.py

---
+++ connects.py
@@ -0,0 +1,23 @@
+import os
+os.system("cls")
+import date time
+import musql.connector
+cnx=mysql.connector.connect
+cursor="root",database="student",password="root")
+ch="  "
+while ch1="0";
+ ch=input("enter option | to connect 0 to stop 2 to clear:")
+ if ch=="2":
+  os.system("c|s")
+  cursor=chx.cursor c
+  query=(select * from test")
+  cursor.execute(query)
+  print("+{}+{}+".format(" "*10," "*20))
+  print("|{:"10}|{:^20}|.format(" "*10," "*20))
+  for(e,n) in cursor:
+    b=str(e)
+    d=b.z fill(4)
+    print("|{:>10}|{:<20}|".format,d,n))
+    print("+{}+{}",format(" "*10," " *20))
+cursor.close()
+chx.close
