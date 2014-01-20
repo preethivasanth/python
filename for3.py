14c72e48d0f25323408627d1edfdaac180c6bc60
preethivasanth <preethiv.cse@gmail.com>
2014-01-20 19:13:50 +0530

    Create for3.py

---
+++ for3.py
@@ -0,0 +1,13 @@
+
+for i in range(5):
+ for j in range(i,5):
+  print(" ",end='')
+ for k in range(i):
+  print(" *",end='')
+ print()
+for i in range(5):
+ for j in range(i):
+  print(" ",end='')
+ for k in range(i,5):
+  print(" *",end='')
+ print()
