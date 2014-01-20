e2176c8042c7e7bc8314c5f801eebba8a8acb864
preethivasanth <preethiv.cse@gmail.com>
2014-01-20 20:51:29 +0530

    Create serverconnect.py

---
+++ serverconnect.py
@@ -0,0 +1,9 @@
+#!/python33/python
+print("Content-type:text/html")
+print(" ")
+print("<html><head>")
+print(" ")
+print("</head><body>")
+print("Hello from python")
+print("<br>I'm web python")
+print("</body></html>")
