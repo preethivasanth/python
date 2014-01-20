7f2ea3ff45b3c00a0621fd7f9e4ba8efd7df239f
preethivasanth <preethiv.cse@gmail.com>
2014-01-20 19:19:21 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,7 @@
+import math
+n=eval(input("enter 1st number"))
+m=eval(input("enter 2nd number"))
+print("square root is",math.sqrt(n))
+print("biggest num is",math.ceil(math.sqrt(n)))
+print("max num is",max(n,m))
+
