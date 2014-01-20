c01c03c10af1515f0934218abd29d0d90901c9b8
preethivasanth <preethiv.cse@gmail.com>
2014-01-20 19:08:49 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,10 @@
+a=eval(input("enter 1st num"))
+b=eval(input("enter 2nd num"))
+c=eval(input("enter 3rd num"))
+if((a>b)&(a>c)):
+ print("a greater")
+elif((b>a)&(b>c)):
+ print("b grater")
+else:
+ print("c greater")
+
